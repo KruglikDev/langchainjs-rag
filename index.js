97f26fb5a57b606a89b174479d72588c0cf4f205
Andrey Kruglik <a.kruglik@keep-calm.ru>
2024-09-15 19:46:54 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import { Ollama } from "@langchain/ollama";
+
+class PdfQA {
+
+    constructor({ model }) {
+        this.model = model;
+    }
+
+    init(){
+        this.initChatModel();
+        return this;
+    }
+
+    // Make this method async in order to use the await keyword inside the function body:
+    async initChatModel(){
+        console.log("Loading model...");
+        this.llm = new Ollama({ model: this.model });
+        // Use the invoke() method available to ask a question to the LLM
+        const response = await this.llm.invoke("Who was the first black man in baseball league?");
+        console.log(response);
+    }
+
+}
+
+const pdfQa = new PdfQA({ model: "llama3" }).init();
